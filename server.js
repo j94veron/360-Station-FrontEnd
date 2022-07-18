c779eff0eabe8bc741282cd9095c63cae098edbe
j94veron <26.veron@gmail.com>
2022-07-18 14:34:25 -0300

    Added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const environments = require ('./environments.json');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('./dist'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/*', (req, res) =>  {
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Server start');
+});
