faef60b9147a5e84054583b66701f644545f6477
j94veron <26.veron@gmail.com>
2022-07-18 16:25:22 -0300

    Mod. Server Browser List

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/*', (req, res) =>  {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html/'));
 });
 
 app.listen(process.env.PORT || 8080, () => {
